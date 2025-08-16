22d376fa13b568413cbd583484a95c06b1adc2f6
Aarav Jain <aaravjain2007@gmail.com>
2025-08-15 23:35:04 -0400

    Chatbot component started

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-
+import Chatbot from "@/components/Chatbot";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold">Welcome to the Marvel App</h1>
         <p className="mt-4 text-lg text-color">Explore the Marvel universe like never before.</p>
+        <Chatbot />
       </div>
     </div>
   );
